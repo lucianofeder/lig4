a2c3b3b8c81806180ea97dbcb31d5f0165d41719
Luciano Feder <lucianoo.feder@gmail.com>
2021-01-05 19:46:10 -0300

    Implementado a verificacao da logica na diagonal

---
+++ js/diagonalCondition.js
@@ -0,0 +1,69 @@
+let arrBoard = [
+    ['_', '_', '_', '_', '_', '_', '_'],
+    ['_', '_',  1 , '_',  1 , '_', '_'],
+    ['_', '_', '_',  1 , '_', '_', '_'],
+    ['_', '_',  1 , '_', '_', '_', '_'],
+    ['_',  1 , '_', '_', '_',  1 , '_'],
+    ['_', '_', '_', '_', '_', '_', '_'],
+  ];
+
+const diag1Verify = (linha, coluna) => {
+    let maxDelta = coluna > linha ? linha : coluna
+    let i = linha - maxDelta
+    let j = coluna - maxDelta
+    let countPlayer = 0
+    console.log(arrBoard[i][j])
+
+    while (arrBoard[i][j]) {
+        console.log(`teste`)
+        if (arrBoard[i][j] === 1) {
+            countPlayer++
+        } else {
+            countPlayer = 0
+        }
+        if (countPlayer === 4) {
+            console.log(`Voce ganhou`)
+        }
+        i++
+        j++
+
+        if (i >= arrBoard.length || j >= arrBoard[0].length) {
+            break
+        }
+    }
+}
+
+const diag2Verify = (linha, coluna) => {
+    let maxDelta = 0
+    let lineLenght = arrBoard[0].length 
+
+    if ((lineLenght - coluna) > linha) {
+        maxDelta = linha
+    } else {
+        maxDelta = lineLenght - colunas
+    }
+
+    let i = linha - maxDelta
+    let j = coluna + maxDelta
+    let countPlayer = 0
+  
+    console.log(arrBoard[i][j])
+
+    while (arrBoard[i][j]) {
+        console.log(`teste`)
+        if (arrBoard[i][j] === 1) {
+            countPlayer++
+        } else {
+            countPlayer = 0
+        }
+        if (countPlayer === 4) {
+            console.log(`Voce ganhou`)
+        }
+        i++
+        j--
+
+        if (i >= arrBoard.length || j <= 0) {
+            break
+        }
+    }
+}
