c589e3313ff92471007583e4139a4466d7167c84
Itallo <dornelaszxd@gmail.com>
2021-01-06 20:24:39 -0300

    Ajustes no css e sombra de clicar

--- js/menuStart.js
+++ js/menuStart.js
@@ -1,5 +1,6 @@
 const button = document.getElementById("startGame");
 const click = () => {
   document.getElementById("menuStart").style.display = "none";
+  document.getElementById("conteiner").style.display = "none";
 };
 button.addEventListener("click", click);
