5216543103bc736344f3009c9cd941c37fa29527
Ursinho Amiguinho <matheus2hcosta@gmail.com>
2021-01-07 12:39:52 -0300

    Implemention of rotacion: Icond and Header Balls. With Associated Names Adjusteds

--- js/playersTurn.js
+++ js/playersTurn.js
@@ -1,16 +1,21 @@
 const turnPlayer = () => {
 
     if(playerTurn===1){
-    turnRound.setAttribute("class", "fas fa-arrow-left")
+        arrowDirection.setAttribute("class", "fas fa-arrow-left fixed-arrow") 
+        player1Balls.setAttribute("class", "headerBall player1Balls rotate-arrow-ball") 
+        player2Balls.setAttribute("class", "headerBall player2Balls")
     }
     if(playerTurn===2){
-    turnRound.setAttribute("class", "fas fa-arrow-right")
+        arrowDirection.setAttribute("class", "fas fa-arrow-left rotate-arrow-ball")
+        player2Balls.setAttribute("class", "headerBall player2Balls rotate-arrow-ball")
+        player1Balls.setAttribute("class", "headerBall player1Balls")
   
     }
  }
 
-let turnRound = document.getElementById("turnRound")
-turnRound.setAttribute("class", "fas fa-arrow-left")
+let arrowDirection = document.querySelector(".fa-arrow-left")
+let player1Balls = document.querySelector(".player1Balls")
+let player2Balls = document.querySelector(".player2Balls")
 
 
 const setPlayerTurn = () => {
